13744330ca48e0f59999d2ce6513bee07278301b
shalalajun <shalalajun@naver.com>
2018-07-17 12:24:34 +0900

    2version

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 function setup() {
-  
+  createCanvas(displayWidth,displayHeight);
   // put setup code here
 }
 
 function draw() {
-
+  background(150);
+  smooth();
+  noStroke();
+  ellipse(width/2,height/2,40,40);
   // put drawing code here
 }
